c030b721b2b5409992ef968ebe4827c291e3be40
Silva <arthur.costa.d.silva@accenture.com>
2017-12-08 11:06:23 -0300

    TODO LIST AULA 4

--- app/modules/router.js
+++ app/modules/router.js
@@ -1,5 +1,14 @@
 function config($stateProvider, $httpProvider, $urlRouterProvider) {
 
+    // ESTUDAR ESSES PROVIDERS
+    $httpProvider.defaults.useXDomain = true;
+    // $httpProvider.defaults.withCredentials =  false;
+    // $httpProvider.defaults.crossDomain = false,
+    $httpProvider.defaults.headers.common = {Accept: "application/json, text/plain, */**"};
+    $httpProvider.defaults.headers.post = {"Content-Type": "application/json;charset=utf-8"};
+    // delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+
     $urlRouterProvider.otherwise('dashboard');
 
     $stateProvider
@@ -18,6 +27,16 @@ function config($stateProvider, $httpProvider, $urlRouterProvider) {
             url: 'todo',
             controller: 'TodoCtrl as vm',
             templateUrl: 'modules/todo/views/todo.html'
+        })
+        .state('main.todo-create', {
+            url: 'todo/create',
+            controller: 'TodoCreateCtrl as vm',
+            templateUrl: 'modules/todo/views/todo.create.html'
+        })
+        .state('main.todo-update', {
+            url: 'todo/update/:id',
+            controller: 'TodoUpdateCtrl as vm',
+            templateUrl: 'modules/todo/views/todo.update.html'
         });
 }
 
--- app/modules/todo/controllers/todo.ctrl.js
+++ app/modules/todo/controllers/todo.ctrl.js
@@ -1,20 +1,37 @@
-function TodoCtrl( TodoFactory ) {
+function TodoCtrl( TodoService, TodoFactory ) {
     var vm = this;
-    vm.pageTitle = 'Todo <small>subtitle</small>';
-    vm.products = {
-        Title: "Product Test",
-        Sub: "Test",
-        Price: 13.54
-    };
-    vm.product = {};
+    vm.pageTitle = 'Todo <small>list</small>';   
+    vm.users = {};
 
+    vm.deleteUserApi = _deleteUserApi;
 
     _init();
 
     function _init() {
-        vm.product = TodoFactory.mockToView( vm.products );
-        console.log(vm.product);
+        TodoService.getApiList().then( _apiSuccess, _apiError );
     }
+
+    function _apiSuccess(response){
+        vm.users = TodoFactory.getApiToView( response );
+    }
+
+    function _apiError(error){
+        
+    }
+
+    function _deleteUserApi( item ){
+        TodoService.deleteUserApi( item.id ).then( function(response){
+            // Pega o parametro para escolher a linha que vai remover da view
+            // Por isso só essa parte nao estar com um method anonymous
+            _deleteSuccess( item );
+        }, _apiError);
+    }
+
+    function _deleteSuccess( item ){
+        var index = vm.users.indexOf(item);
+        vm.users.splice(index, 1);
+    }
+
 }
 
 angular.module('todoApp')
